b6bf8ef2d75185fa503850da357ad997d4ade7a6
gulsina <gulsina.zh>
2022-08-04 22:56:22 +0600

    feat: added NotFound.js

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import styles from './NotFound.module.css';
 
 const NotFound = () => {
     return (
-        <div className={styles.not_found_title}>
-            <h1>Страничка не найдена</h1>
+        <div className={styles.not_found}>
+            <Link to="/">
+                <img
+                    src="https://static.vecteezy.com/system/resources/previews/000/534/948/original/404-error-page-not-found-concept-in-white-background-for-sites-under-constructions-vector.jpg"
+                    alt=""/>
+            </Link>
         </div>
     );
 };
